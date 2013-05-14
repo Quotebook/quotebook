a7a6011c6e6a5339946134938bc20b5a4df4d2bd
Caleb Fisher <caleb@z2live.com>
2013-05-14 00:36:58 -0700

    removing non submodule core

--- iosClientCode/submodules/core/base/Base.h
+++
@@ -1,8 +0,0 @@
-#import "Asserts.h"
-
-#import "NSObject+Object.h"
-#import "NSObject+Serialization.h"
-#import "Utilities.h"
-#import <UIKit/UIKit.h>
-#import <UIKit/UINib.h>
-#import <QuartzCore/QuartzCore.h>
